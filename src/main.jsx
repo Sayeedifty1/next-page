b1bb9047f936911b183c320352bbbed5bb53d793
Sayeedifty1 <md.abusayeedifty@gmail.com>
2023-04-06 02:43:11 +0600

    book details added

--- src/main.jsx
+++ src/main.jsx
@@ -3,27 +3,35 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
 import About from './Components/About'
+import BookDetails from './Components/BookDetails'
 import Books from './Components/Books'
 import Home from './Components/Home'
 import './index.css'
 
 const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App></App>,
-    children:[
+    path: '/',
+    element: <App></App>,
+    children: [
       {
-        path:'/',
-        element:<Home></Home>
+        path: '/',
+        element: <Home></Home>
       },
       {
-        path:'books',
-        element:<Books></Books>,
-        loader: ()=> fetch('https://api.itbook.store/1.0/new'),
+        path: 'books',
+        element: <Books></Books>,
+        loader: () => fetch('https://api.itbook.store/1.0/new'),
       },
       {
-        path:'about',
-        element:<About></About>
+        path: 'book/:id',
+        element: <BookDetails></BookDetails>,
+        loader: ({ params }) =>
+        fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+
+      },
+      {
+        path: 'about',
+        element: <About></About>
       }
     ]
   }
@@ -33,6 +41,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode> 
-    <RouterProvider router={router}></RouterProvider>
-  
+  <RouterProvider router={router}></RouterProvider>
+
 )
